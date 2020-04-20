bc00f0dd92133290f27405b882a86dede41c6c57
Vasiliy Varakin <bukawandr@gmail.com>
2020-04-20 19:26:00 +0400

    add GetAll

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
@@ -26,6 +27,11 @@ type repository interface {
 type Repository struct {
 	mu           sync.RWMutex
 	consignments []*pb.Consignment
+}
+
+// GetAll consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Create a new consignment
@@ -59,6 +65,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	// Return matching the `Response` message we created in our
 	// protobuf definition.
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+// GetConsignments -
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
